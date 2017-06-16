96c54585a203bd1bbe734440a8d860abff40042a
Nadir Hamid <nadir@ubuntu-1gb-nyc2-01.localdomain>
2017-06-16 02:19:49 +0000

    Remove a test

--- test.py
+++
@@ -1,8 +0,0 @@
-from swim.swim import Swim
-from swim.swim_server import SwimServer
-from swim.swim_server_handler import SwimServerHandler
-swim = Swim({"hosts": ["127.0.0.1:5901"]})
-splitted = swim.opts.local.split(":")
-server = SwimServer((splitted[0], int(splitted[1]),), SwimServerHandler, swim.opts)
-server.serve_forever()
-
